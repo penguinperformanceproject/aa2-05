532c58f74e41148df74aa141357577ee1403d270
penguinperformanceproject <55525189+penguinperformanceproject@users.noreply.github.com>
2020-09-09 16:25:07 +0900

    Add files via upload

---
+++ aa2-05-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what) {
+  console.log("Hello " + what + "!");
+}
+
+hello("aa05");
+hello("lsh20151542, 이승현");
---
+++ aa2-05-rpt02/start/hello_my_module.js
@@ -0,0 +1,4 @@
+var olleh = require("./hello_module.js");
+
+olleh("aa05");
+olleh("Node");
