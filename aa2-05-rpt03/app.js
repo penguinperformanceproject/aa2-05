a628aeaf5ff56011100affd488dc99781247dd03
penguinperformanceproject <55525189+penguinperformanceproject@users.noreply.github.com>
2020-09-16 17:33:56 +0900

    Add files via upload

---
+++ aa2-05-rpt03/app.js
@@ -0,0 +1,27 @@
+// app.js
+var express = require("express");
+var app = express();
+var port = 3000;
+
+app.get("/", function (req, res){
+    res.send('<a href="/hello">Hello Page</a>');
+});
+
+app.get("/hello", function (req, res){
+    res.send("Hello aa05");
+});
+app.get("/comsi", function (req, res){
+    res.send("Hello Comsi!");
+});
+app.get("/aa05", function (req, res){
+    res.send("Hello aa05, Comsi! My first express server!!!");
+});
+app.get("/naver", function (req, res){
+    res.send('<a href="http://www.naver.com">Go to naver</a>');
+});
+
+
+//listening requests from clients
+var server = app.listen(port, function() {
+    console.log("Listening on port %d", server.address().port);
+});
